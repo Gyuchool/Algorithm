360a91def8d3ea294e60e016f68d8462f13c220f
giron <60054318+Gyuchool@users.noreply.github.com>
2021-04-14 18:51:23 +0900

    acmicpc_1927

---
+++ baekjoon/최소힙.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+priority_queue<int> pq;
+
+int main() {
+
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
+
+	int index = 0;
+	int n,x;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> x;
+		if (x == 0) {
+			if (pq.size()==0) {
+				cout << 0 << '\n';
+			}
+			else {
+				cout << -pq.top() << '\n';
+				pq.pop();
+			}
+			
+		}
+		else {
+			pq.push(-x);
+		}
+	}
+}
