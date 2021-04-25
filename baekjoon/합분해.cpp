77ac6a06fdc4eacaba7ee5496d9d86d3f3957987
giron <60054318+Gyuchool@users.noreply.github.com>
2021-04-25 13:53:45 +0900

    acmicpc_2225

---
+++ baekjoon/합분해.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int dp[201][201];
+
+int main() {
+	int n, k;
+
+	cin >> n >> k;
+	
+	for (int i = 1; i <= n; ++i) {
+		dp[i][1] = 1;
+	}
+
+	for (int i = 1; i <= k; ++i) {
+		dp[1][i] = i;
+	}
+	for (int i = 2; i <= n; ++i) {
+		for (int j = 2; j <= k; ++j) {
+			dp[i][j] = (dp[i][j - 1] + dp[i - 1][j])%1000000000;
+		}
+	}
+
+	cout << dp[n][k] % 1000000000;
+
+}
