766a4e1ff343bb4c16634b87473d45ee49c162d0
giron <60054318+Gyuchool@users.noreply.github.com>
2022-08-10 01:23:48 +0900

    2번째_풀이

--- 프로그래머스/카카오/문자열압축.cpp
+++ 프로그래머스/카카오/문자열압축.cpp
@@ -43,3 +43,53 @@ int solution(string s) {
     }
     return answer;
 }
+---
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> v;
+
+int solution(string s) {
+       for (int i = 1; i <= s.size() / 2+1; ++i) {
+        string standard = s.substr(0, i);
+        int k=0;
+        int cnt = 0;
+        string tmp = "";
+        for (int j = 0; j < s.size(); j += i) {
+            if (s.substr(j, i) == standard) {
+                cnt++;
+            } else {
+                if (j + i >= s.size()) {
+                    k=j;
+                    break;
+                }
+                if (cnt == 1) {
+                    tmp += standard;
+                } else {
+                    tmp += to_string(cnt) + standard;
+                }
+                cnt = 1;
+                standard = s.substr(j, i);
+            }
+        }
+        if (cnt == 1) {
+            tmp += standard;
+        } else {
+            tmp += to_string(cnt) + standard;
+        }
+        if(k){
+            tmp += s.substr(k);
+        }
+        v.push_back(tmp);
+    }
+    int min =9876543210;
+    
+    for(int i=0; i<v.size(); i++){
+        if (min > v[i].size()) {
+            min = v[i].size();
+        }
+    }
+    return min;
+}
