7b0747d24aa7475bc1fb02199247c860b70a28d4
giron <60054318+Gyuchool@users.noreply.github.com>
2023-04-18 13:23:45 +0900

    우선순위 큐

---
+++ baekjoon/절댓값 힙.py
@@ -0,0 +1,14 @@
+import heapq
+
+N = int(input())
+pq = []
+
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if pq:
+            print(heapq.heappop(pq)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(pq, (abs(x), x))
